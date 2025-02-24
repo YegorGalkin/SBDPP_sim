1119cb7c82a6984b190e0066d42f6e2e2d07baf8
Michael Artemov <msartyomov@edu.hse.ru>
2025-02-24 22:14:13 +0300

    update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
-from setuptools import setup, Extension
+import os
+import sys
+import sysconfig
+
 from distutils.ccompiler import new_compiler
 from distutils.errors import DistutilsPlatformError
-from Cython.Build import cythonize
-import sys
+from setuptools import Extension, find_packages, setup
+
+try:
+    from Cython.Build import cythonize
+except ImportError:
+    sys.exit("Error: Cython is required.")
 
 
 def check_compiler_type():
@@ -12,22 +19,33 @@ def check_compiler_type():
     except DistutilsPlatformError:
         sys.exit("Error: No valid C++ compiler found.")
 
+def get_compile_args():
+    args = []
+    platform = sysconfig.get_platform()
+    
+    if platform.startswith("win"):
+        args.append("/std:c++20")
+    elif platform.startswith(("linux", "darwin")):
+        args.extend(["-std=c++20", "-O3"])
+    else:
+        raise RuntimeError(f"Unsupported platform: {platform}")
+    
+    return args
 
 compiler_type = check_compiler_type()
-if compiler_type == "msvc":
-    compile_args = ["/std:c++20"]
-else:
-    compile_args = ["-std=c++20"]
+compile_args = get_compile_args()
 
 extensions = [
     Extension(
-        # 1) Remove dot to avoid mismatch in init symbol
         name="simulation.SpatialBirthDeath",
-        sources=["simulation/SpatialBirthDeathWrapper.pyx",
-                 "simulation/SpatialBirthDeath.cpp"],
+        sources=[
+            "simulation/SpatialBirthDeathWrapper.pyx",
+            "src/SpatialBirthDeath.cpp"
+        ],
         language="c++",
-        include_dirs=["include"],
-        extra_compile_args=compile_args
+        include_dirs=[os.path.abspath("include")],
+        extra_compile_args=compile_args,
+        extra_link_args=compile_args
     )
 ]
 
@@ -35,7 +53,6 @@ setup(
     name="spatial_sim",
     version="1.0",
     description="Spatial birth-death point process simulator",
-    ext_modules=cythonize(extensions, annotate=True,
-                          compiler_directives={"language_level": "3"}),
-    packages=["SpatialBirthDeath"],
+    ext_modules=cythonize(extensions, annotate=True, compiler_directives={"language_level": "3"}),
+    packages=find_packages(),
 )
