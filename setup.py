af86a035c645d2acd752564d2be3207e457f6691
Michael Artemov <msartyomov@edu.hse.ru>
2025-03-01 19:00:12 +0300

    remove distutils

--- setup.py
+++ setup.py
@@ -1,29 +1,28 @@
 import os
-import sys
-
-from distutils.ccompiler import new_compiler
-from distutils.errors import DistutilsPlatformError
+import sysconfig
 from setuptools import Extension, find_packages, setup
 from Cython.Build import cythonize
 
 
-def check_compiler_type():
-    try:
-        compiler = new_compiler()
-        return compiler.compiler_type
-    except DistutilsPlatformError:
-        sys.exit("Error: No valid C++ compiler found.")
+def get_compiler_args():
+    compiler = sysconfig.get_config_var("CC")
+
+    compiler_str_lower = "" if compiler is None else compiler.lower()
+    if "msvc" in compiler_str_lower:
+        # Use /O2 for maximum optimization on MSVC
+        compile_args = ["/std:c++20", "/O2"]
+        link_args = [] # MSVC typically does not need optimization flags for linking
+    elif "gcc" in compiler_str_lower or "clang" in compiler_str_lower:
+        # For GCC/Clang, add -O3 and -march=native for maximum optimization
+        compile_args = ["-std=c++20", "-O3", "-march=native"]
+        link_args = compile_args
+    else:
+        raise RuntimeError(f"Unsupported compiler: {compiler}. Only MSVC, GCC, and Clang are supported.")
+        
+    return compile_args, link_args
 
 
-compiler_type = check_compiler_type()
-if compiler_type == "msvc":
-    # Use /O2 for maximum optimization on MSVC
-    compile_args = ["/std:c++20", "/O2"]
-    link_args = []  # MSVC typically does not need optimization flags for linking
-else:
-    # For GCC/Clang, add -O3 and -march=native for maximum optimization
-    compile_args = ["-std=c++20", "-O3", "-march=native"]
-    link_args = compile_args
+compile_args, link_args = get_compiler_args()
 
 
 extensions = [
